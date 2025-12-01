b9faf05c1bb18cd17d5303987cc869ac9e489dd5
alex.dou <alex.dou@qq.com>
2025-12-01 23:31:19 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # www
 Authentication,Authorization,Access Control,Permission Control
+
+### Identity
+>登录凭证
+
+### Security User
+>账号
+ 
+### Role
+>角色
+
+### Permission
+>权限
+
+### Menu
+>菜单
+
+### Project
+>按程序设计menu
+
+---
+登录凭证 -> 账号 -> 角色 -> 权限列表 -> 菜单
+
+---
---
+++ examples/internal/etc/config.models.go
@@ -0,0 +1,47 @@
+package etc
+
+type Configuration struct {
+	Db      db      `toml:"db"`
+	Web     web     `toml:"web"`
+	Token   token   `toml:"token"`
+	Redis   redis   `toml:"redis"`
+	Console console `toml:"console"`
+	Sender  sender  `toml:"sender"`
+}
+
+type web struct {
+	Listen string `toml:"listen"`
+}
+
+type redis struct {
+	Enable   bool   `toml:"enable"`
+	Addr     string `toml:"addr"`
+	Password string `toml:"password"`
+	Db       int    `toml:"db"`
+}
+
+type token struct {
+	Enable bool   `toml:"enable"`
+	Issuer string `toml:"issuer"`
+	Key    string `toml:"key"`
+	Ttl    int    `toml:"ttl"`
+}
+
+type sender struct {
+	Strategy int `toml:"strategy"` // task.Strategy
+	Buff     int `toml:"buff"`
+}
+
+type db struct {
+	User     string `toml:"user"`
+	Password string `toml:"password"`
+	Host     string `toml:"host"`
+	Port     int    `toml:"port"`
+	Database string `toml:"database"`
+	Ssl      string `toml:"ssl"`
+}
+
+// console独有的配置
+type console struct {
+	TokenProvider string `toml:"tokenProvider"`
+}
